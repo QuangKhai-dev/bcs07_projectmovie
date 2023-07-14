2e5e1c8baea7027bdb60a5b5edd86a205a3a6586
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2023-07-14 12:17:01 +0700

    admin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import UserTemplate from './template/UserTemplate';
 import HomePage from './pages/HomePage/HomePage';
 import Page404 from './pages/Page404/Page404';
 import Login from './pages/Login/Login';
+import AdminTemplate from './template/AdminTemplate';
+import LoginAdmin from './pages/LoginAdmin/LoginAdmin';
+import UserManagement from './pages/UserManagement/UserManagement';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="/login" element={<Login />} />
         </Route>
+        <Route path="/admin" element={<AdminTemplate />}>
+          <Route index element={<UserManagement />} />
+        </Route>
+        <Route path="/admin-login" element={<LoginAdmin />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
